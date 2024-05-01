5881d138aa370404822de7d453349b9b3be6657c
CodingRafi <codingmuhamadrafiprasetya.gmail.com>
2024-05-01 22:10:08 +0700

    seeting seeder

--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -15,45 +15,45 @@ class SettingSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('settings')->insert([
-            'nama' => 'Uang Tranport Dosen',
-            'type' => 'number',
-            'value' => '15000',
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
+        // DB::table('settings')->insert([
+        //     'nama' => 'Uang Tranport Dosen',
+        //     'type' => 'number',
+        //     'value' => '15000',
+        //     'created_at' => now(),
+        //     'updated_at' => now()
+        // ]);
 
-        DB::table('settings')->insert([
-            'nama' => 'URL Neofeeder',
-            'type' => 'text',
-            'value' => '',
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
+        // DB::table('settings')->insert([
+        //     'nama' => 'URL Neofeeder',
+        //     'type' => 'text',
+        //     'value' => '',
+        //     'created_at' => now(),
+        //     'updated_at' => now()
+        // ]);
 
-        DB::table('settings')->insert([
-            'nama' => 'Uang Tranport Asdos',
-            'type' => 'number',
-            'value' => '15000',
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
+        // DB::table('settings')->insert([
+        //     'nama' => 'Uang Tranport Asdos',
+        //     'type' => 'number',
+        //     'value' => '15000',
+        //     'created_at' => now(),
+        //     'updated_at' => now()
+        // ]);
 
-        DB::table('settings')->insert([
-            'nama' => 'Uang Teori Dosen 1 SKS',
-            'type' => 'number',
-            'value' => '15000',
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
+        // DB::table('settings')->insert([
+        //     'nama' => 'Uang Teori Dosen 1 SKS',
+        //     'type' => 'number',
+        //     'value' => '15000',
+        //     'created_at' => now(),
+        //     'updated_at' => now()
+        // ]);
 
-        DB::table('settings')->insert([
-            'nama' => 'Uang Teori Asdos 1 SKS',
-            'type' => 'number',
-            'value' => '15000',
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
+        // DB::table('settings')->insert([
+        //     'nama' => 'Uang Teori Asdos 1 SKS',
+        //     'type' => 'number',
+        //     'value' => '15000',
+        //     'created_at' => now(),
+        //     'updated_at' => now()
+        // ]);
 
         DB::table('settings')->insert([
             'nama' => 'Uang Praktek Dosen 1 SKS',
