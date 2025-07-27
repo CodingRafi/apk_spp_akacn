25b302fa98dc9016c7631fd4836581cc21e9deaf
CodingRafi <codingmuhamadrafiprasetya.gmail.com>
2025-07-27 12:09:27 +0700

    cronjob

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('jadwal:close')->dailyAt('00:00');
+        $schedule->command('jadwal:close')->dailyAt('00:00');
     }
 
     /**
