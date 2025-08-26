929d0bc7b700de2a798ba91c081c1ccc949e47a1
CodingRafi <codingmuhamadrafiprasetya.gmail.com>
2025-08-26 23:24:16 +0700

    refactor: kuesioner

--- app/Http/Controllers/Mahasiswa/KuesionerController.php
+++ app/Http/Controllers/Mahasiswa/KuesionerController.php
@@ -9,6 +9,19 @@ use Illuminate\Support\Facades\DB;
 class KuesionerController extends Controller
 {
     public function store(Request $request){
+        //? Cek udh pernah ngisi belom
+        $checkSudahIsi = DB::table('t_kuesioners')
+        ->where('mhs_id', auth()->user()->id)
+        ->where('tahun_semester_id', $request->input('tahun_semester_id'))
+        ->where('tahun_matkul_id', $request->input('tahun_matkul_id'))
+        ->first();
+
+        if ($checkSudahIsi) {
+            return response()->json([
+                'message' => 'Anda sudah mengisi kuesioner ini'
+            ], 400);
+        }
+        
         $cek = $request->except(['_token', 'tahun_matkul_id', 'tahun_semester_id']);
 
         $kuesioner = DB::table('kuesioners')
@@ -32,15 +45,17 @@ class KuesionerController extends Controller
             'updated_at' => now()
         ]);
 
+        $answers = [];
         foreach ($cek as $key => $value) {
-            DB::table('t_kuesioners_answer')->insert([
+            $answers[] = [
                 't_kuesioner_id' => $tKuesioner,
                 'kuesioner_id' => $key,
                 'answer' => $value,
                 'created_at' => now(),
-                'updated_at' => now()
-            ]);
+                'updated_at' => now(),
+            ];
         }
+        DB::table('t_kuesioners_answer')->insert($answers);
 
         return response()->json([
             'message' => 'Berhasil disimpan'
